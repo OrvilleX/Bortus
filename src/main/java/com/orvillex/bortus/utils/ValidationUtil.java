962e125450eacfc262e2f5e17fdcb649ba063dde
姚正发 <976691141@qq.com>
2020-11-19 16:44:36 +0800

    完成ValidationUtil工具开发

---
+++ src/main/java/com/orvillex/bortus/utils/ValidationUtil.java
@@ -0,0 +1,30 @@
+package com.orvillex.bortus.utils;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.orvillex.bortus.exception.BadRequestException;
+import org.hibernate.validator.internal.constraintvalidators.hv.EmailValidator;
+
+/**
+ * 验证工具类
+ * @author y-z-f
+ * @version 0.1
+ */
+public class ValidationUtil {
+    
+    /**
+     * 验证空
+     */
+    public static void isNull(Object obj, String entity, String parameter, Object value) {
+        if (ObjectUtil.isNull(obj)) {
+            String msg = entity + " 不存在：" + parameter + " is " + value;
+            throw new BadRequestException(msg); 
+        }
+    }
+
+    /**
+     * 验证是否为邮箱
+     */
+    public static boolean isEmail(String email) {
+        return new EmailValidator().isValid(email, null);
+    }
+}
