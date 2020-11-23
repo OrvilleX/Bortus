880179828fbabf84d6e2b94b1212343670e27be0
姚正发 <976691141@qq.com>
2020-11-23 23:43:09 +0800

    完成日志枚举与注解

---
+++ src/main/java/com/orvillex/bortus/enums/LogActionType.java
@@ -0,0 +1,23 @@
+package com.orvillex.bortus.enums;
+
+import lombok.Getter;
+
+/**
+ * 日志类型
+ * @author y-z-f
+ * @version 0.1
+ */
+@Getter
+public enum LogActionType {
+
+    ADD("新增"),
+    SELECT("查询"),
+    UPDATE("更新"),
+    DELETE("删除");
+
+    private String value;
+
+    LogActionType(String value) {
+        this.value = value;
+    }
+}
