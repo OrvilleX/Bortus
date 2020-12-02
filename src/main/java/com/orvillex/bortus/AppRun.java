4f2970c4ee4d2ec52a8955a96d0df2a3bb2f21cf
姚正发 <976691141@qq.com>
2020-12-02 11:53:01 +0800

    完成验证管理API

--- src/main/java/com/orvillex/bortus/AppRun.java
+++ src/main/java/com/orvillex/bortus/AppRun.java
@@ -1,23 +1,8 @@
-/*
- *  Copyright 2019-2020 Zheng Jie
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-package me.zhengjie;
+package com.orvillex.bortus;
 
+import com.orvillex.bortus.annotation.AnonymousGetMapping;
+import com.orvillex.bortus.handler.SpringContextHolder;
 import io.swagger.annotations.Api;
-import me.zhengjie.annotation.rest.AnonymousGetMapping;
-import me.zhengjie.utils.SpringContextHolder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
@@ -29,10 +14,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 开启审计功能 -> @EnableJpaAuditing
- *
- * @author Zheng Jie
- * @date 2018/11/15 9:20:19
+ * 主入口
+ * @author y-z-f
+ * @version 0.1
  */
 @EnableAsync
 @RestController
@@ -60,8 +44,6 @@ public class AppRun {
 
     /**
      * 访问首页提示
-     *
-     * @return /
      */
     @AnonymousGetMapping("/")
     public String index() {
