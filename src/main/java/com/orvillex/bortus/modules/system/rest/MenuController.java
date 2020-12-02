3efb18ab77e4331bacdbeb7e40f55c61db204765
姚正发 <976691141@qq.com>
2020-12-02 11:01:41 +0800

    完成菜单管理API

--- src/main/java/com/orvillex/bortus/modules/system/rest/MenuController.java
+++ src/main/java/com/orvillex/bortus/modules/system/rest/MenuController.java
@@ -1,60 +1,44 @@
-/*
- *  Copyright 2019-2020 Zheng Jie
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-package me.zhengjie.modules.system.rest;
+package com.orvillex.bortus.modules.system.rest;
 
 import cn.hutool.core.collection.CollectionUtil;
+import com.orvillex.bortus.annotation.Log;
+import com.orvillex.bortus.exception.BadRequestException;
+import com.orvillex.bortus.modules.system.domain.Menu;
+import com.orvillex.bortus.modules.system.service.MenuService;
+import com.orvillex.bortus.modules.system.service.automap.MenuMapper;
+import com.orvillex.bortus.modules.system.service.dto.MenuDto;
+import com.orvillex.bortus.modules.system.service.dto.MenuQueryCriteria;
+import com.orvillex.bortus.utils.PageUtil;
+import com.orvillex.bortus.utils.SecurityUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import me.zhengjie.annotation.Log;
-import me.zhengjie.modules.system.domain.Menu;
-import me.zhengjie.exception.BadRequestException;
-import me.zhengjie.modules.system.service.MenuService;
-import me.zhengjie.modules.system.service.dto.MenuDto;
-import me.zhengjie.modules.system.service.dto.MenuQueryCriteria;
-import me.zhengjie.modules.system.service.mapstruct.MenuMapper;
-import me.zhengjie.utils.PageUtil;
-import me.zhengjie.utils.SecurityUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
 /**
- * @author Zheng Jie
- * @date 2018-12-03
+ * 菜单管理API
+ * @author y-z-f
+ * @version 0.1
  */
-
 @RestController
 @RequiredArgsConstructor
 @Api(tags = "系统：菜单管理")
 @RequestMapping("/api/menus")
 public class MenuController {
-
     private final MenuService menuService;
     private final MenuMapper menuMapper;
-    private static final String ENTITY_NAME = "menu";
 
     @Log("导出菜单数据")
     @ApiOperation("导出菜单数据")
     @GetMapping(value = "/download")
-    @PreAuthorize("@el.check('menu:list')")
+    @PreAuthorize("@x.check('menu:list')")
     public void download(HttpServletResponse response, MenuQueryCriteria criteria) throws Exception {
         menuService.download(menuService.queryAll(criteria, false), response);
     }
@@ -64,12 +48,12 @@ public class MenuController {
     public ResponseEntity<Object> buildMenus(){
         List<MenuDto> menuDtoList = menuService.findByUser(SecurityUtils.getCurrentUserId());
         List<MenuDto> menuDtos = menuService.buildTree(menuDtoList);
-        return new ResponseEntity<>(menuService.buildMenus(menuDtos),HttpStatus.OK);
+        return new ResponseEntity<>(menuService.buildMenus(menuDtos), HttpStatus.OK);
     }
 
     @ApiOperation("返回全部的菜单")
     @GetMapping(value = "/lazy")
-    @PreAuthorize("@el.check('menu:list','roles:list')")
+    @PreAuthorize("@x.check('menu:list','roles:list')")
     public ResponseEntity<Object> query(@RequestParam Long pid){
         return new ResponseEntity<>(menuService.getMenus(pid),HttpStatus.OK);
     }
@@ -77,7 +61,7 @@ public class MenuController {
     @Log("查询菜单")
     @ApiOperation("查询菜单")
     @GetMapping
-    @PreAuthorize("@el.check('menu:list')")
+    @PreAuthorize("@x.check('menu:list')")
     public ResponseEntity<Object> query(MenuQueryCriteria criteria) throws Exception {
         List<MenuDto> menuDtoList = menuService.queryAll(criteria, true);
         return new ResponseEntity<>(PageUtil.toPage(menuDtoList, menuDtoList.size()),HttpStatus.OK);
@@ -86,7 +70,7 @@ public class MenuController {
     @Log("查询菜单")
     @ApiOperation("查询菜单:根据ID获取同级与上级数据")
     @PostMapping("/superior")
-    @PreAuthorize("@el.check('menu:list')")
+    @PreAuthorize("@x.check('menu:list')")
     public ResponseEntity<Object> getSuperior(@RequestBody List<Long> ids) {
         Set<MenuDto> menuDtos = new LinkedHashSet<>();
         if(CollectionUtil.isNotEmpty(ids)){
@@ -102,10 +86,10 @@ public class MenuController {
     @Log("新增菜单")
     @ApiOperation("新增菜单")
     @PostMapping
-    @PreAuthorize("@el.check('menu:add')")
+    @PreAuthorize("@x.check('menu:add')")
     public ResponseEntity<Object> create(@Validated @RequestBody Menu resources){
         if (resources.getId() != null) {
-            throw new BadRequestException("A new "+ ENTITY_NAME +" cannot already have an ID");
+            throw new BadRequestException("不能携带ID");
         }
         menuService.create(resources);
         return new ResponseEntity<>(HttpStatus.CREATED);
@@ -114,7 +98,7 @@ public class MenuController {
     @Log("修改菜单")
     @ApiOperation("修改菜单")
     @PutMapping
-    @PreAuthorize("@el.check('menu:edit')")
+    @PreAuthorize("@x.check('menu:edit')")
     public ResponseEntity<Object> update(@Validated(Menu.Update.class) @RequestBody Menu resources){
         menuService.update(resources);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -123,7 +107,7 @@ public class MenuController {
     @Log("删除菜单")
     @ApiOperation("删除菜单")
     @DeleteMapping
-    @PreAuthorize("@el.check('menu:del')")
+    @PreAuthorize("@x.check('menu:del')")
     public ResponseEntity<Object> delete(@RequestBody Set<Long> ids){
         Set<Menu> menuSet = new HashSet<>();
         for (Long id : ids) {
