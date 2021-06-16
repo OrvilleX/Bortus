86548bce204b513722d0cc15bf5bb0d9cc9f36ef
姚正发 <976691141@qq.com>
2021-06-16 23:55:35 +0800

    增加参数模型注释方式

--- bortus-manager/src/main/java/com/orvillex/bortus/manager/modules/system/domain/vo/LoginByCodeVo.java
+++ bortus-manager/src/main/java/com/orvillex/bortus/manager/modules/system/domain/vo/LoginByCodeVo.java
@@ -1,5 +1,7 @@
 package com.orvillex.bortus.manager.modules.system.domain.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,16 +12,34 @@ import lombok.NoArgsConstructor;
  * @version 0.1
  */
 @Data
+@ApiModel(value = "微信登录凭据")
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginByCodeVo {
-    String code;
-    String nickName;
-    Integer gender;
-    String city;
-    String province;
-    String country;
-    String avatarUrl;
-    String encryptedData;
-    String iv;
+    @ApiModelProperty(value = "微信鉴权码")
+    private String code;
+
+    @ApiModelProperty(value = "昵称")
+    private String nickName;
+
+    @ApiModelProperty(value = "性别")
+    private Integer gender;
+
+    @ApiModelProperty(value = "城市")
+    private String city;
+
+    @ApiModelProperty(value = "省份")
+    private String province;
+
+    @ApiModelProperty(value = "区")
+    private String country;
+
+    @ApiModelProperty(value = "头像地址")
+    private String avatarUrl;
+
+    @ApiModelProperty(value = "加密数据")
+    private String encryptedData;
+
+    @ApiModelProperty(value = "偏移量")
+    private String iv;
 }
